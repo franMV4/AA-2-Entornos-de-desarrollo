2502b36e6ca52367aa0b0f5293edce7be15ebeef
franMV4 <79657510+franMV4@users.noreply.github.com>
2021-04-01 21:33:39 +0200

    La aplicaion esta hecha

---
+++ src/com/SanValero/Aplicacion.java
@@ -0,0 +1,24 @@
+package com.SanValero;
+
+import com.SanValero.Cliente;
+import com.SanValero.Empleado;
+import com.SanValero.Ordenador;
+
+public class Aplicacion {
+    public static void main(String[]args){
+        Ordenador ordenador;
+        Cliente cliente;
+        Empleado empleado;
+
+    ordenador = new Ordenador("Intel", "z40", 754, "Ram", true, "Intel");
+    cliente = new Cliente("Jorge", "Argualas", 34, true, "Maria");
+    empleado = new Empleado("Adrian", "74642478C", 67, false, "75348545H");
+
+    ordenador.getCausa();
+
+
+    ordenador.piezasMarcaIguales();
+    cliente.nombresIguales();
+    empleado.DniAsociadosTarjeta();
+    }
+}
